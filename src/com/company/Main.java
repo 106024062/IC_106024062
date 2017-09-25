a2ef65b97f1378449457554a044145134c8d6661
106024062 <kirsty01240302@yahoo.com.tw>
2017-09-25 14:46:31 +0800

    106024062

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("106024062");// write your code here
+        // System.out.print("資傳系");
+        }
+}
